f542aa2b20ea525c40640397e7975b0ef447843f
DoanHieu <doanhieu.bl.hn@gmail.com>
2020-01-12 00:17:10 +0700

    tesk insert into

--- src/main/java/DoanHieu/dao/GenericDAO.java
+++ src/main/java/DoanHieu/dao/GenericDAO.java
@@ -7,5 +7,8 @@ import DoanHieu.mapper.RowMapper;
 public interface GenericDAO<T> {
 	//nos se co nhieu  tham so nhung chua can dung  ngay, ta co the dung de truyen  nhieu  tham so  moi vao ,co  luong tham so  khong co dinh 
 		<T>List<T> query(String sql, RowMapper<T> rowMapper ,Object ... paramenters);
+		//cap nhat và xoa 
+		void update(String sql ,Object ... parameters );
+		Long insert (String sql, Object... parameters);
 		
 }
--- src/main/java/DoanHieu/dao/impl/AbstractDAO.java
+++ src/main/java/DoanHieu/dao/impl/AbstractDAO.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -72,11 +73,94 @@ public class AbstractDAO<T> implements GenericDAO<T> {
 					statement.setLong(index,(Long) paramenter);
 				} else if(paramenter instanceof String ) {
 					statement.setString(index,(String) paramenter);
+				} else if(paramenter instanceof Integer) {
+					statement.setInt(index,(Integer)paramenter);
+				}else if(paramenter instanceof Timestamp) {
+					statement.setTimestamp(index, (Timestamp)paramenter);
 				}
 			}
 		}catch(SQLException e) {
 			e.printStackTrace();
 		}
 		
+	}
+
+	@Override
+	public void update(String sql, Object... parameters) {
+	//co 3 ddoi tuong 
+		Connection connection = null;
+		PreparedStatement statement = null;
+		try {
+			 connection =getConnection();
+			 connection.setAutoCommit(false); 
+				statement =connection.prepareStatement(sql);
+				setParameter(statement, parameters);
+				statement.executeUpdate();
+				connection.commit();
+		}catch(SQLException e) {
+			if(connection !=null) {
+				try {
+					connection.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}finally {
+			try {	if(connection !=null) {
+				connection.close();
+			}
+			if(statement !=null) {
+				statement.close();
+			}
+			
+			}catch(SQLException e2) {
+				e2.printStackTrace();
+			}
+		
+		}	
+	}
+
+	@Override
+	public Long insert(String sql, Object... parameters) {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			Long id =null ;
+			 connection =getConnection();
+			 connection.setAutoCommit(false); 
+				statement =connection.prepareStatement(sql,statement.RETURN_GENERATED_KEYS);
+				setParameter(statement, parameters);
+				statement.executeUpdate();
+				resultSet = statement.getGeneratedKeys();
+				if(resultSet.next()) {
+					id= resultSet.getLong(1);
+				}
+				connection.commit();
+				return id;
+		}catch(SQLException e) {
+			if(connection !=null) {
+				try {
+					connection.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}finally {
+			try {	if(connection !=null) {
+				connection.close();
+			}
+			if(statement !=null) {
+				statement.close();
+			}if(resultSet !=null) {
+				resultSet.close();
+			}
+			
+			}catch(SQLException e2) {
+				e2.printStackTrace();
+			}
+		
+		}	
+		return null;
 	}
 }
